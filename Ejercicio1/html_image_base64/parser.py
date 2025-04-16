a5565ef93bd3cfe37468241df4bd5f95eb95a11a
David Lopez <david.lopezcuervov@gmail.com>
2025-04-15 22:24:04 -0500

    Ejercicio 1: parser.py recorre todo el html para extraer las imagenes

---
+++ Ejercicio1/html_image_base64/parser.py
@@ -0,0 +1,17 @@
+from html.parser import HTMLParser
+
+class HTMLImageParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.img_sources = []
+
+    def handle_starttag(self, tag, attrs):
+        # Detectamos los <img>
+        if tag.lower() == "img":
+            attrs_dict = dict(attrs)
+            src = attrs_dict.get("src")
+            if src:
+                self.img_sources.append(src)
+
+    def get_img_sources(self):
+        return self.img_sources
